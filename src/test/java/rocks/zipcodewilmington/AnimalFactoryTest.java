6149a3d046aea614551cd1f0bf72f82207fe15b5
k-romero <imkevinromero@gmail.com>
2020-02-14 17:42:35 -0500

    completed animal factory tests, need to finish CatHouse & DogHouse

--- src/test/java/rocks/zipcodewilmington/AnimalFactoryTest.java
+++ src/test/java/rocks/zipcodewilmington/AnimalFactoryTest.java
@@ -3,6 +3,7 @@ package rocks.zipcodewilmington;
 import com.sun.jndi.toolkit.ctx.AtomicDirContext;
 import org.junit.Assert;
 import org.junit.Test;
+import rocks.zipcodewilmington.animals.Cat;
 import rocks.zipcodewilmington.animals.Dog;
 import rocks.zipcodewilmington.animals.animal_creation.AnimalFactory;
 
@@ -21,8 +22,19 @@ public class AnimalFactoryTest {
     public void createDogTest() {
         String expected = "orbit";
 
-         Dog testDog = AnimalFactory.createDog(expected, new Date(02343));
+         Dog testDog = AnimalFactory.createDog(expected, new Date(0001));
          String actual = testDog.getName();
+
+        Assert.assertEquals(expected,actual);
+
+    }
+
+    @Test
+    public void createCatTest() {
+        String expected = "Whiskers";
+
+        Cat testCat = AnimalFactory.createCat(expected, new Date(1999));
+        String actual = testCat.getName();
 
         Assert.assertEquals(expected,actual);
 
